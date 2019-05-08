c49d611d6760bd61bb17674873ad0a42053a50b1
Irving Popovetsky <irving@chef.io>
2019-05-08 14:51:12 -0700

    Minor cleanups for base image definition, and updating the owner ID for who publishes those
    Signed-off-by: Irving Popovetsky <irving@chef.io>

--- scripts/disable_selinux.sh
+++ scripts/disable_selinux.sh
@@ -2,5 +2,5 @@
 set -o errexit -o nounset -o pipefail
 
 echo ">>> Disabling SELinux"
-sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+sed -i 's/^SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 setenforce 0
