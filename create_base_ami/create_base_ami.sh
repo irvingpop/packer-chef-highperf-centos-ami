8956d4a99db1c54b55ea9c13e7a08552be2d96a6
Irving Popovetsky <irving@chef.io>
2019-10-07 15:32:01 -0700

    initial attempt at adding CentOS 8 Stream
    Signed-off-by: Irving Popovetsky <irving@chef.io>

--- create_base_ami/create_base_ami.sh
+++ create_base_ami/create_base_ami.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
-### Procedure to create an image on an additional volume on an existing instance:
-#
-# - Launch a CentOS AMI, make sure it is a newer type that uses nvme disks (e.g. T3 or M5)
-#   - At launch time, specify attaching a secondary volume that is 8GB.  Do not set the secondary volume to delete on termination.
-# - Log in to your instance and become root (sudo -i)
-# - Update your instance, especially if it is not the latest (< 7.6):  yum upgrade -y
-# - run: curl -O https://raw.githubusercontent.com/irvingpop/packer-chef-highperf-centos7-ami/marketplace/create_base_ami.sh
-# - Find the volume using "lsblk". It's probaly named "nvme1n1"
-# - export DEVICE="/dev/nvme1n1" # export the DEVICE variable for this script
-# - bash -x create_base_ami.sh # start this script
-#
-# Wait until the script has completed. Can can take 10 minutes or so.
-#
-# When complete, convert the $DEVICE into an AMI by creating a snapshot of the
-# EBS volume and converting the snapshot into an AMI.  These steps can be done
-# with the AWS web console or using the CLI tools.
-#
-# How to create an AMI of the volume?
-#
-# - Detach the additional volume from the instance in the EC2 Dashboard menu
-#   Volumes.
-# - Create a snapshot of the detached volume by selecting it, and executing the
-#   action "Create Snapshot". Provide a useful description for the snapshot.
-# - Create an AMI of the created snapshot in the EC2 Dashboard menu Snapshots
-#   by selecting the snapshot and executing the action "Create Image". Provide
-#   the following values:
-#   - Name: Useful, short name.
-#   - Description: Description, more verbose, including for example the script name and repo URL used to create it.
-#   - Virtualisation type: Hardware-assisted vistualisation
-#
-
 yum upgrade -y
 
 ROOTFS=/rootfs
--- create_base_ami/create_base_ami8.sh
+++ create_base_ami/create_base_ami8.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
-set -o errexit -o nounset -o pipefail
+set -o errexit -o pipefail
 
-### Procedure to create an image on an additional volume on an existing instance:
-#
-# - Launch a CentOS AMI, make sure it is a newer type that uses nvme disks (e.g. T3 or M5)
-#   - At launch time, specify attaching a secondary volume that is 8GB.  Do not set the secondary volume to delete on termination.
-# - Log in to your instance and become root (sudo -i)
-# - Update your instance, especially if it is not the latest:  yum upgrade -y
-# - run: curl -O https://raw.githubusercontent.com/irvingpop/packer-chef-highperf-centos7-ami/marketplace/create_base_ami.sh
-# - Find the volume using "lsblk". It's probaly named "nvme1n1"
-# - export DEVICE="/dev/nvme1n1" # export the DEVICE variable for this script
-# - bash -x create_base_ami.sh # start this script
-#
-# Wait until the script has completed. Can can take 10 minutes or so.
-#
-# When complete, convert the $DEVICE into an AMI by creating a snapshot of the
-# EBS volume and converting the snapshot into an AMI.  These steps can be done
-# with the AWS web console or using the CLI tools.
-#
-# How to create an AMI of the volume?
-#
-# - Detach the additional volume from the instance in the EC2 Dashboard menu
-#   Volumes.
-# - Create a snapshot of the detached volume by selecting it, and executing the
-#   action "Create Snapshot". Provide a useful description for the snapshot.
-# - Create an AMI of the created snapshot in the EC2 Dashboard menu Snapshots
-#   by selecting the snapshot and executing the action "Create Image". Provide
-#   the following values:
-#   - Name: Useful, short name.
-#   - Description: Description, more verbose, including for example the script name and repo URL used to create it.
-#   - Virtualisation type: Hardware-assisted vistualisation
-#
+while getopts ":s:" opt; do
+  case $opt in
+    s) CENTOS_STREAM=$OPTARG ;;
+  esac
+done
+
+if [[ "${CENTOS_STREAM}" == "true" ]]; then
+  release_pkg_url="https://mirrors.edge.kernel.org/centos/8-stream/BaseOS/x86_64/os/Packages/centos-release-stream-8.0-0.1905.0.9.el8.x86_64.rpm"
+  echo "Building CentOS 8 Stream"
+else
+  release_pkg_url="https://mirrors.edge.kernel.org/centos/8.0.1905/BaseOS/x86_64/os/Packages/centos-release-8.0-0.1905.0.9.el8.x86_64.rpm"
+  echo "Building CentOS 8.0"
+fi
 
 yum upgrade -y
 
@@ -51,8 +34,7 @@ mkdir -p "$ROOTFS"
 mount "$PARTITION" "$ROOTFS"
 
 rpm --root="$ROOTFS" --initdb
-rpm --root="$ROOTFS" --nodeps -ivh \
-  https://mirrors.edge.kernel.org/centos/8.0.1905/BaseOS/x86_64/os/Packages/centos-release-8.0-0.1905.0.9.el8.x86_64.rpm
+rpm --root="$ROOTFS" --nodeps -ivh $release_pkg_url
 yum --installroot="$ROOTFS" --nogpgcheck -y update
 yum --installroot="$ROOTFS" --nogpgcheck -y groupinstall "Minimal Install" \
   --exclude="iwl*firmware" \
